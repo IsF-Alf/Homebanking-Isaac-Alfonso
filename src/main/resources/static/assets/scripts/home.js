c9d6fb0a5a59739337167ac7a5b4da16c0c3c24c
Isaac Alfonso <ifa24991@gmail.com>
2023-09-30 19:52:08 -0300

    Sprint 1

--- src/main/resources/static/assets/scripts/home.js
+++ src/main/resources/static/assets/scripts/home.js
@@ -3,10 +3,39 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      message: "Hello Vue!",
+      firstName: "",
+      lastName: "",
+      email: "",
+      clientsInformation: [],
+      clientsData: [],
+      respuestaJSON: "",
     };
   },
-  created() {},
-  methods() {},
+  created() {
+    axios
+      .get("/clients")
+      .then((response) => {
+        apiResp = response.data._embedded.clients;
+        this.clientsInformation = apiResp;
+        this.respuestaJSON = response.data;
+        console.log(apiResp);
+      })
+      .catch((error) => console.log(error));
+  },
+  methods: {
+    addUser() {
+      const clientsData = {
+        firstName: this.firstName,
+        lastName: this.lastName,
+        email: this.email,
+      };
+      axios
+        .post("/clients", clientsData)
+        .then((response) => {
+          alert("Usuario creado");
+          console.log(response);
+        })
+        .catch((error) => console.log(error));
+    },
+  },
 }).mount("#app");
-
