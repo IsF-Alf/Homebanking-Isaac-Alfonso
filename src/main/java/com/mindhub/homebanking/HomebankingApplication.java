33a1967eda8391dccb64ecd1deac072ac67688fa
Isaac Alfonso <ifa24991@gmail.com>
2023-10-05 02:28:40 -0300

    Sprint 2

--- src/main/java/com/mindhub/homebanking/HomebankingApplication.java
+++ src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -1,11 +1,15 @@
 package com.mindhub.homebanking;
 
+import com.mindhub.homebanking.models.Account;
 import com.mindhub.homebanking.models.Client;
+import com.mindhub.homebanking.repositories.AccountRepository;
 import com.mindhub.homebanking.repositories.ClientRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class HomebankingApplication {
@@ -15,10 +19,18 @@ public class HomebankingApplication {
     }
 
     @Bean
-    public CommandLineRunner initData(ClientRepository clientRepository) {
+    public CommandLineRunner initData(ClientRepository clientRepository, AccountRepository accountRepository) {
         return args -> {
             Client client = new Client("Isaac", "Alfonso", "ifa24991@gmail.com");
             clientRepository.save(client);
+            Client melba = new Client("Melba", "Morel","melba_m@gmail.com");
+            clientRepository.save(melba);
+            Account account1 = new Account("VIN001", LocalDate.now(),5000.00);
+            Account account2 = new Account("VIN002", LocalDate.now().plusDays(1),7500.00);
+            melba.addAccount(account1);
+            melba.addAccount(account2);
+            accountRepository.save(account1);
+            accountRepository.save(account2);
         };
     }
 }
---
+++ src/main/java/com/mindhub/homebanking/dto/ClientDTO.java
@@ -0,0 +1,42 @@
+package com.mindhub.homebanking.dto;
+
+import com.mindhub.homebanking.models.Client;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ClientDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private List<AccountDTO> accounts;
+
+    public ClientDTO(Client client) {
+        id = client.getId();
+        firstName = client.getFirstName();
+        lastName = client.getLastName();
+        email = client.getEmail();
+        accounts= client.getAccounts().stream().map(account -> new AccountDTO(account)).collect(Collectors.toList());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public List<AccountDTO> getAccounts() {
+        return accounts;
+    }
+}
