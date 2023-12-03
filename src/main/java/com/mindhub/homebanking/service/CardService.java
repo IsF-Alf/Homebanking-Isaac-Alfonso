1068d5f75372f624322b691b39639bdc0653c604
Isaac <ifa24991@gmail.com>
2023-12-02 22:57:28 -0300

    modificacion card

--- src/main/java/com/mindhub/homebanking/service/CardService.java
+++ src/main/java/com/mindhub/homebanking/service/CardService.java
@@ -13,5 +13,5 @@ public interface CardService {
     Card findById (Long id);
     List<Card> findAll();
     Card findByNumber(String number);
-    boolean existByCvv (int cvv);
+    boolean existsByCvv (int cvv);
 }
